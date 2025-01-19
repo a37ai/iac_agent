bd620a7e614d85706c300ada50e9fd46eb6fd723
Your Name (forge) <you@example.com>
2025-01-18 21:52:24 -0500

    feat: Add new EC2 instance resource to main.tf using default configurations

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_instance" "new_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = var.name
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
