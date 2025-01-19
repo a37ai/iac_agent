0dd3c0afd13a5bc1c28291890a011e3580d96314
Your Name (forge) <you@example.com>
2025-01-18 20:06:12 -0500

    feat: Add EC2 instance configuration for development environment in main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_instance" "dev_instance" {
+  ami           = var.dev_ami_id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.development_key.key_name
+  tags = {
+    Name = "DevelopmentInstance"
+  }
+}
+
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
